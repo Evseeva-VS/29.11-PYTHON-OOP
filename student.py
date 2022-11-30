57b7f9fd9624f6249fbe7a7b0f119cda3bbb84a0
210User <210User@pgk63.ru>
2022-11-30 09:06:47 +0400

    Добавлен перевод студентов на следующий курс

--- student.py
+++ student.py
@@ -17,3 +17,14 @@ class Student(object):
         self.email = email
         self.curs = curs
         self.grades = grades
+
+    # Перевести на курс выше
+    def go_to_next_curs(self):
+        # Если студент 5 курса - не переводим
+        if (self.curs == 5): return
+        self.curs += 1
+
+student = Student('','',0,'','',5)
+print(student.curs)
+student.go_to_next_curs()
+print(student.curs)
