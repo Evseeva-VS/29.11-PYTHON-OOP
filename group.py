579d5a540feeed8471a394e3b3042dd24aca2e6e
210User <210User@pgk63.ru>
2022-11-30 09:52:36 +0400

    Добавлено сравнение групп по количеству студентов

--- group.py
+++ group.py
@@ -18,3 +18,11 @@ class Group(object):
     def get_by_gender(self, gender):
         return '\n'.join([f'{student.fio}, {student.age} лет, оценки: {student.grades}' for student in filter(lambda x: x.gender == gender, [student for student in self.students])])
     
+    # Сравнение групп по количеству студентов
+    def students_comparison(self, instance):
+        if (len(self.students) > len(instance.students)):
+            return f'В группе {self.name}-{self.number} больше студентов чем в группе {instance.name}-{instance.number} на {len(self.students) - len(instance.students)}'
+        elif (self.curs < instance.curs):
+            return f'В группе {instance.name}-{instance.number} больше студентов чем в группе {self.name}-{self.number} на {len(instance.students) - len(self.students)}'
+        else:
+            return f'В группах одинаковое количество студентов'
